eba40d6a0ab72ed4e98c565c3064633f3ee6f205
Victor Rentea <victorrentea@gmail.com>
2023-09-13 12:48:19 +0300

    surpriza!!

--- src/main/java/victor/perf/ReduceMultithreadBenchmark.java
+++ src/main/java/victor/perf/ReduceMultithreadBenchmark.java
@@ -7,6 +7,7 @@ import rx.Observable;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
 
 import static java.lang.Math.sqrt;
@@ -27,20 +28,41 @@ public class ReduceMultithreadBenchmark {
    }
 
 
+   // 300
    @Benchmark
-   public int reduceSerial() {
-      return numbers.stream().reduce(0, Integer::sum);
+   public int sumParallel() {
+      return numbers.parallelStream().mapToInt(i->i).sum();
    }
-   @Benchmark
-   public int reduceParallel() {
-      return numbers.parallelStream().reduce(0, Integer::sum);
-   }
+
+   // 600
    @Benchmark
    public int sumSerial() {
       return numbers.stream().mapToInt(i->i).sum();
    }
    @Benchmark
-   public int sumParallel() {
-      return numbers.parallelStream().mapToInt(i->i).sum();
+   public int reduceParallel() {
+      return numbers.parallelStream().reduce(0, Integer::sum);
+   }
+
+   // 2600
+   @Benchmark
+   public int reduceSerial() {
+      return numbers.stream().reduce(0, Integer::sum);
+   }
+
+   // 7000
+   @Benchmark
+   public int atomicSerial() {
+      AtomicInteger atomicInteger = new AtomicInteger();
+      numbers.forEach(atomicInteger::addAndGet);
+      return atomicInteger.get();
+   }
+
+   // 52000
+   @Benchmark
+   public int atomicParallel() {
+      AtomicInteger atomicInteger = new AtomicInteger();
+      numbers.parallelStream().forEach(atomicInteger::addAndGet);
+      return atomicInteger.get();
    }
 }

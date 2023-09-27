057aa76f83c87bd703be8470e43b82e259fc38e7
Victor Rentea <victorrentea@gmail.com>
2023-09-27 20:01:08 +0300

    if vf optional

---
+++ src/main/java/victor/perf/If_vs_Optional.java
@@ -0,0 +1,58 @@
+package victor.perf;
+
+import lombok.Data;
+import org.openjdk.jmh.annotations.*;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+import rx.Observable;
+
+import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
+
+import static java.lang.Math.sqrt;
+
+@State(Scope.Thread)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@Warmup(iterations = 5, time = 100, timeUnit = TimeUnit.MILLISECONDS)
+@Measurement(iterations = 10, time = 100, timeUnit = TimeUnit.MILLISECONDS)
+@Fork(1)
+public class If_vs_Optional {
+  // https://stackoverflow.com/questions/56235254/optional-vs-if-else-if-performance-java-8
+
+
+  private <T> Optional<T> wrap(T o) {
+    return Optional.ofNullable(o);
+  }
+
+  @Benchmark
+  public String optionalChain() {
+    Message message = new Message();
+    return Stream.of(message.getA(), message.getB(), message.getC(), message.getD())
+        .filter(Objects::nonNull)
+        .findFirst()
+        .orElse(null);
+
+  }
+
+  @Benchmark
+  public Object ifChain() {
+    Message message = new Message();
+    if (message.getA() != null)
+      return message.getA();
+    else if (message.getB() != null)
+      return message.getB();
+    else if (message.getC() != null)
+      return message.getC();
+    else if (message.getD() != null)
+      return message.getD();
+    else return null;
+  }
+
+  @Data
+  public static class Message {
+    String a, b, c, d, e, f, g;
+  }
+}
--- src/main/java/victor/perf/ReduceMultithreadBenchmark.java
+++ src/main/java/victor/perf/ReduceMultithreadBenchmark.java
@@ -1,68 +1,68 @@
-package victor.perf;
-
-import org.openjdk.jmh.annotations.*;
-import reactor.core.publisher.Flux;
-import reactor.core.scheduler.Schedulers;
-import rx.Observable;
-
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.IntStream;
-
-import static java.lang.Math.sqrt;
-import static java.util.stream.Collectors.toList;
-
-@State(Scope.Thread)
-@BenchmarkMode(Mode.AverageTime)
-@OutputTimeUnit(TimeUnit.MICROSECONDS)
-@Warmup(iterations = 5, time = 200, timeUnit = TimeUnit.MILLISECONDS)
-@Measurement(iterations = 15, time = 200, timeUnit = TimeUnit.MILLISECONDS)
-@Fork(1)
-public class ReduceMultithreadBenchmark {
-   private List<Integer> numbers;
-
-   @Setup
-   public void createNumbersList() {
-      numbers = IntStream.range(0, 1_000_000).boxed().collect(toList());
-   }
-
-
-   // 300
-   @Benchmark
-   public int sumParallel() {
-      return numbers.parallelStream().mapToInt(i->i).sum();
-   }
-
-   // 600
-   @Benchmark
-   public int sumSerial() {
-      return numbers.stream().mapToInt(i->i).sum();
-   }
-   @Benchmark
-   public int reduceParallel() {
-      return numbers.parallelStream().reduce(0, Integer::sum);
-   }
-
-   // 2600
-   @Benchmark
-   public int reduceSerial() {
-      return numbers.stream().reduce(0, Integer::sum);
-   }
-
-   // 7000
-   @Benchmark
-   public int atomicSerial() {
-      AtomicInteger atomicInteger = new AtomicInteger();
-      numbers.forEach(atomicInteger::addAndGet);
-      return atomicInteger.get();
-   }
-
-   // 52000
-   @Benchmark
-   public int atomicParallel() {
-      AtomicInteger atomicInteger = new AtomicInteger();
-      numbers.parallelStream().forEach(atomicInteger::addAndGet);
-      return atomicInteger.get();
-   }
-}
+//package victor.perf;
+//
+//import org.openjdk.jmh.annotations.*;
+//import reactor.core.publisher.Flux;
+//import reactor.core.scheduler.Schedulers;
+//import rx.Observable;
+//
+//import java.util.List;
+//import java.util.concurrent.TimeUnit;
+//import java.util.concurrent.atomic.AtomicInteger;
+//import java.util.stream.IntStream;
+//
+//import static java.lang.Math.sqrt;
+//import static java.util.stream.Collectors.toList;
+//
+//@State(Scope.Thread)
+//@BenchmarkMode(Mode.AverageTime)
+//@OutputTimeUnit(TimeUnit.MICROSECONDS)
+//@Warmup(iterations = 5, time = 200, timeUnit = TimeUnit.MILLISECONDS)
+//@Measurement(iterations = 15, time = 200, timeUnit = TimeUnit.MILLISECONDS)
+//@Fork(1)
+//public class ReduceMultithreadBenchmark {
+//   private List<Integer> numbers;
+//
+//   @Setup
+//   public void createNumbersList() {
+//      numbers = IntStream.range(0, 1_000_000).boxed().collect(toList());
+//   }
+//
+//
+//   // 300
+//   @Benchmark
+//   public int sumParallel() {
+//      return numbers.parallelStream().mapToInt(i->i).sum();
+//   }
+//
+//   // 600
+//   @Benchmark
+//   public int sumSerial() {
+//      return numbers.stream().mapToInt(i->i).sum();
+//   }
+//   @Benchmark
+//   public int reduceParallel() {
+//      return numbers.parallelStream().reduce(0, Integer::sum);
+//   }
+//
+//   // 2600
+//   @Benchmark
+//   public int reduceSerial() {
+//      return numbers.stream().reduce(0, Integer::sum);
+//   }
+//
+//   // 7000
+//   @Benchmark
+//   public int atomicSerial() {
+//      AtomicInteger atomicInteger = new AtomicInteger();
+//      numbers.forEach(atomicInteger::addAndGet);
+//      return atomicInteger.get();
+//   }
+//
+//   // 52000
+//   @Benchmark
+//   public int atomicParallel() {
+//      AtomicInteger atomicInteger = new AtomicInteger();
+//      numbers.parallelStream().forEach(atomicInteger::addAndGet);
+//      return atomicInteger.get();
+//   }
+//}

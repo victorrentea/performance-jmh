ad1b117ed90fca3aad25d58473908fd34c4bedd1
Victor Rentea <victorrentea@gmail.com>
2024-11-07 12:58:04 +0200

    jmh ftw

---
+++ src/main/java/victor/perf/StreamVsFor_NAIVE.java
@@ -0,0 +1,30 @@
+package victor.perf;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class StreamVsFor_NAIVE {
+  public static void main(String[] args) {
+    // create the collection before the bench
+    List<Integer> numbers = IntStream.range(0, 1000).boxed().toList();
+
+    long start = System.currentTimeMillis();
+    long sum = 0;
+    for (int element : numbers) {
+      sum += element;
+    }
+    System.out.println("For: " + (System.currentTimeMillis() - start) + "ms");
+    System.out.println("Sum1: " + sum);
+
+    start = System.currentTimeMillis();
+    sum = numbers.stream().mapToInt(i -> i).sum();
+    System.out.println("Stream: " + (System.currentTimeMillis() - start) + "ms");
+    System.out.println("Sum2: " + sum);
+  }
+}
+// issues:
+// - no of iterations
+// - GC
+// - order of experiments
+// - lack of warmup
+// - no repeated runs

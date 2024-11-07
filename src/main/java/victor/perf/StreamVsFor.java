071c950191a633311b88a95ab8f8340882166adf
Victor Rentea <victorrentea@gmail.com>
2024-11-07 12:56:09 +0200

    jmh ftw

--- src/main/java/victor/perf/StreamVsFor.java
+++ src/main/java/victor/perf/StreamVsFor.java
@@ -1,92 +1,91 @@
-//package victor.perf;
-//
-//import org.openjdk.jmh.annotations.*;
-//import org.openjdk.jmh.infra.Blackhole;
-//
-//import java.util.List;
-//import java.util.concurrent.TimeUnit;
-//import java.util.stream.IntStream;
-//
-//import static java.util.concurrent.TimeUnit.MILLISECONDS;
-//import static java.util.stream.Collectors.toList;
-//
-//@Fork(1)
-//@State(Scope.Thread)
-//@BenchmarkMode(Mode.AverageTime) // or throughput calls/sec,...
-//@OutputTimeUnit(TimeUnit.MICROSECONDS) // or nano,...
-//
-//// throw away runs prior to the measurements to allow JIT to optimize code
-//@Warmup(iterations = 3, time = 100, timeUnit = MILLISECONDS)
-// between runs, JMH also invokes GC
-//
-//// during which call in a loop the @Benchmarks bellow
-//@Measurement(iterations = 5, time = 100, timeUnit = MILLISECONDS)
-//public class StreamVsFor {
-//
-//  @Param({"100", "10000"})
-//  int collection_size;
-//
-//  @Param({"LOW", "HEAVY"})
-//  Cpu cpu;
-//
-//  public enum Cpu {LOW, HEAVY}
-//
-//  long cpuTokens;
-//
-//  private List<Integer> numbers;
-//
-//  @Setup
-//  public void createNumbersList() {
-//    numbers = IntStream.range(0, collection_size).boxed().collect(toList());
-//    cpuTokens = cpu == Cpu.LOW ? 10 : 100;
-//  }
-//
-//  @Benchmark
-//  public void _0_oneTask() {
-//    Blackhole.consumeCPU(cpuTokens); // calibrating a task
-//  }
-//
-//  @Benchmark
-//  public int _1_forIndex() { // pre-java 5
-//    int sum = 0;
-//    for (int i = 0; i < numbers.size(); i++) {
-//      if (numbers.get(i) % 2 == 0) {
-//        sum += cpuOnlyTask(numbers.get(i));
-//      }
-//    }
-//    return sum;
-//  }
-//
-//  @Benchmark
-//  public int _2_for5() {
-//    int sum = 0;
-//    for (int n : numbers) {
-//      if (n % 2 == 0) {
-//        sum += cpuOnlyTask(n);
-//      }
-//    }
-//    return sum;
-//  }
-//
-//  @Benchmark
-//  public long _3_stream() {
-//    return numbers.stream()
-//        .filter(n -> n % 2 == 0)
-//        .mapToInt(n -> cpuOnlyTask(n))
-//        .sum();
-//  }
-//
-//  @Benchmark
-//  public long _4_parallelStream() {
-//    return numbers.parallelStream()
-//        .filter(n -> n % 2 == 0)
-//        .mapToInt(this::cpuOnlyTask)
-//        .sum();
-//  }
-//
-//  private int cpuOnlyTask(int n) {
-//    Blackhole.consumeCPU(cpuTokens);
-//    return n;
-//  }
-//
-//}
+package victor.perf;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.util.stream.Collectors.toList;
+
+@Fork(1)
+@State(Scope.Thread)
+@BenchmarkMode({Mode.AverageTime, /* Mode.SampleTime */}) // uncomment for more percentiles
+@OutputTimeUnit(TimeUnit.MICROSECONDS) // or nano,...
+
+// throw away runs prior to the measurements to allow JIT to optimize code
+@Warmup(iterations = 3, time = 100, timeUnit = MILLISECONDS)
+//between runs, JMH runs GC to apublic void pauses during experiments
+
+// during which call in a loop the @Benchmarks bellow
+@Measurement(iterations = 5, time = 100, timeUnit = MILLISECONDS)
+public class StreamVsFor {
+
+  @Param({"100", "10000"})
+  private int list_size;
+
+  public enum Cpu {LOW, HEAVY}
+  @Param({"LOW", "HEAVY"})
+  public Cpu cpu;
+
+  private long cpuTokens;
+
+  private List<Integer> numbers;
+
+  @Setup
+  public void createNumbersList() {
+    numbers = IntStream.range(0, list_size).boxed().collect(toList());
+    cpuTokens = cpu == Cpu.LOW ? 7 : 100;
+  }
+
+  @Benchmark
+  public int _1_for_i() { // pre-java 5
+    int sum = 0;
+    for (int i = 0; i < numbers.size(); i++) {
+      if (numbers.get(i) % 2 == 0) {
+        sum += cpuOnlyTask(numbers.get(i));
+      }
+    }
+    return sum;
+  }
+
+  @Benchmark
+  public int _2_for() {
+    int sum = 0;
+    for (int n : numbers) {
+      if (n % 2 == 0) {
+        sum += cpuOnlyTask(n);
+      }
+    }
+    return sum;
+  }
+
+  @Benchmark
+  public long _3_stream() {
+    return numbers.stream()
+        .filter(n -> n % 2 == 0)
+        .mapToInt(n -> cpuOnlyTask(n))
+        .sum();
+  }
+
+  @Benchmark
+  public long _4_parallelStream() {
+    return numbers.parallelStream()
+        .filter(n -> n % 2 == 0)
+        .mapToInt(this::cpuOnlyTask)
+        .sum();
+  }
+
+  private int cpuOnlyTask(int n) {
+    Blackhole.consumeCPU(cpuTokens);
+    return n;
+  }
+
+  @Benchmark
+  public void _0_work_for_one_element() {
+    Blackhole.consumeCPU(cpuTokens); // calibrating work/element
+  }
+
+}
